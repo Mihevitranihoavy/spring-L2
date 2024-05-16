5dc2f7a0e73e196731d712affd03894acf034fa4
Mihevitranihoavy <solomandresymaxime@gmail.com>
2024-05-16 21:48:52 +0200

    first commit sprint1

--- sprint-code/src/main/java/org/example/frontcontroller/FrontControllerServlet.java
+++ sprint-code/src/main/java/org/example/frontcontroller/FrontControllerServlet.java
@@ -1,18 +1,36 @@
 package org.example.frontcontroller;
 
+import org.example.framework.AnnotationController;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@AnnotationController
 public class FrontControllerServlet extends HttpServlet{
 
     protected void processRequest(HttpServletRequest request , HttpServletResponse response)
             throws ServletException ,IOException{
         String url = request.getRequestURI();
-        response.getWriter().write( "Vous ete sur L'URl :" + url);
+
+        //Scanner les controlleur
+
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+        try{
+            Class<?>[] classes = loader.loadClass("org.example.frontcontroller.FrontControllerServlet").getClasses();
+            for(Class<?> clazz : classes){
+                if(clazz.isAnnotationPresent(AnnotationController.class)){
+                    System.out.println("Controlleur trouvé : "+ clazz.getName());
+                }
+            }
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        response.getWriter().write("Vous etes sur l'URL :"+url);
 
 
     }
